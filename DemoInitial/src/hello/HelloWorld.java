0e6d2b3dd8f1a2ae296dfb08890f656958955b95
Karen González <69605681+karenfggutierrez@users.noreply.github.com>
2024-03-25 10:02:39 -0600

    Added DemoInitial project

---
+++ DemoInitial/src/hello/HelloWorld.java
@@ -0,0 +1,36 @@
+package hello;
+
+import java.security.PublicKey;
+
+import javax.swing.plaf.ToolBarUI;
+
+/**
+ * 
+ * @author karegonz
+ *
+ *@version
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+	
+	
+	/**
+	 * 
+	 * @param i
+	 * @param someFloat
+	 */
+	public void SomeMethod(int i, float someFloat) {
+		
+		ToolBarUI
+		
+		
+		
+		
+		
+	}
+
+}
