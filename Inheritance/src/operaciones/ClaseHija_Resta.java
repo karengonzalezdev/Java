2fbc8b4ec341da50b2fc9dd9368d266f06e0cae4
Karen González <69605681+karenfggutierrez@users.noreply.github.com>
2024-03-25 09:28:37 -0600

    Added Inheritance project

---
+++ Inheritance/src/operaciones/ClaseHija_Resta.java
@@ -0,0 +1,7 @@
+package operaciones;
+
+public class ClaseHija_Resta extends ClasePadre {
+	public void Restar() {
+		resultado = valor1 - valor2;
+	}
+}
---
+++ Inheritance/src/operaciones/ClaseHija_Suma.java
@@ -0,0 +1,7 @@
+package operaciones;
+
+public class ClaseHija_Suma extends ClasePadre {
+	public void Sumar() {
+		resultado = valor1 + valor2;
+	}
+}
